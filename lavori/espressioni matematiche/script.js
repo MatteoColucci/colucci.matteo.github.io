90ddaa284e45aea2ad760e2e602cca959709aed7
MatteoColucci <colucci.matteo@itiscannizzaro.edu.it>
2023-03-16 10:10:15 +0100

    retta peogressiva

--- lavori/espressioni matematiche/script.js
+++ lavori/espressioni matematiche/script.js
@@ -28,13 +28,45 @@ function disegnaRetta() {
     creaAssi();
     
     // Disegna la retta
-    context.beginPath();
-    context.strokeStyle = "red";
-    // y = m * x
-    context.moveTo(0, canvas.height / 2 + m * canvas.width / 2);
-    context.lineTo(canvas.width, canvas.height / 2 - m * canvas.width / 2);
-    context.stroke();
+    
+    const startX = -canvas.width / 2;
+    const startY = m * startX;
+    
+    const endX = canvas.width / 2;
+    const endY = m * endX;
+    
+    var p = 0;
+    var intervallo = setInterval(function() {
+        context.beginPath();
+        context.moveTo(startX + canvas.width / 2, -startY + canvas.height / 2);
+        var x = startX + p * (endX - startX);
+        var y = startY + p * (endY - startY);
+        context.lineTo(x + canvas.width / 2, -y + canvas.height / 2);
+        context.strokeStyle = "red";
+        context.stroke();
+        p += 0.01;
+        if (p > 1) {
+            clearInterval(intervallo);
+        }
+    }, 10);
 }
+
+// function disegnaRetta() {
+//     // Recupera il coefficiente angolare dalla pagina
+//     var m = document.getElementById("coefficiente").value;
+
+//     // Cancella il canvas
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//     creaAssi();
+    
+//     // Disegna la retta
+//     context.beginPath();
+//     context.strokeStyle = "red";
+//     // y = m * x
+//     context.moveTo(0, canvas.height / 2 + m * canvas.width / 2);
+//     context.lineTo(canvas.width, canvas.height / 2 - m * canvas.width / 2);
+//     context.stroke();
+// }
 
 function disegnaCirconferenza() {
     var raggio = document.getElementById("raggio").value * 10;
