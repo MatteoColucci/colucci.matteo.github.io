4a29023f6b651412d104b84fe3ed04351922d238
MatteoColucci <colucci.matteo@itiscannizzaro.edu.it>
2023-10-09 11:00:13 +0200

    aggiunta quinta trimestre

---
+++ lavori/lettura_file_csv/script.js
@@ -0,0 +1,24 @@
+function creaTabella() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://matteocolucci.github.io/main/lavori/tabella-csv.csv", true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const csvContent = this.responseText;
+            const rows = csvContent.split('\n');
+            const tableContainer = document.getElementById('container');
+            let tableHTML = '<table border="1">';
+            for (let i = 0; i < rows.length; i++) {
+                const cells = rows[i].split(',');
+                tableHTML += '<tr>';
+                for (let j = 0; j < cells.length; j++) {
+                    tableHTML += '<td>' + cells[j] + '</td>';
+                }
+                tableHTML += '</tr>';
+            }
+            tableHTML += '</table>';
+            tableContainer.innerHTML = tableHTML;
+        }
+    };
+}
